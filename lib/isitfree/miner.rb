94c79cd3596ed194fcb40707eadbc8cf54e19d8c
Ile Eftimov <ileeftimov@gmail.com>
2014-04-29 01:02:53 +0200

    change binary and improve messages

--- lib/isitfree/miner.rb
+++ lib/isitfree/miner.rb
@@ -1,12 +1,14 @@
+require 'curb'
+
 module Isitfree
   class Miner
     class << self
       def find name
         status = Curl.get("https://www.rubygems.org/gems/#{name}").status
         if status == '200 OK'
-          false
+          "The gem #{name} exists. Sorry :("
         else
-          true
+          "Yay! The gem #{name} does not exist! It's all yours!"
         end
       end
     end
